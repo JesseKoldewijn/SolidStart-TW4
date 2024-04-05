f875628b9ed0e961fee7f3667590c90212bc52f0
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-04-05 08:38:31 +0200

    fix: small tweak of theme change btn (lock on transition state)

--- src/components/theme/ThemeToggle.tsx
+++ src/components/theme/ThemeToggle.tsx
@@ -1,3 +1,5 @@
+import { createSignal } from "solid-js";
+
 import { useThemeStore } from "~/stores/theme.js";
 import { Theme } from "~/types/themes.js";
 
@@ -5,19 +7,37 @@ import { DarkModeIcon, LightModeIcon } from "../icons/Theme.jsx";
 
 const ThemeToggle = () => {
   const store = useThemeStore();
+
+  const [isTransitioning, setIsTransitioning] = createSignal(false);
+
   const theme = store((state) => state.theme);
   const setTheme = store((state) => state.setTheme);
+
+  const handleTransitionStart = (
+    e: MouseEvent & {
+      currentTarget: HTMLButtonElement;
+      target: Element;
+    },
+  ) => {
+    setIsTransitioning(true);
+    handleThemeChange(e, theme(), setTheme);
+
+    setTimeout(() => {
+      setIsTransitioning(false);
+    }, 500);
+  };
 
   return (
     <button
       class="bg-foreground text-background border-foreground flex h-10 w-10 cursor-pointer items-center justify-center rounded-md border-2 p-1"
-      onClick={(e) => handleThemeChange(e, theme(), setTheme)}
+      onClick={handleTransitionStart}
       data-current={theme()}
       aria-label="Toggle light/dark theme"
       style={{
         // @todo remove when cursor pointer is fixed in tw4
         cursor: "pointer",
       }}
+      disabled={isTransitioning()}
     >
       {theme() === "light" ? <DarkModeIcon /> : <LightModeIcon />}
     </button>
